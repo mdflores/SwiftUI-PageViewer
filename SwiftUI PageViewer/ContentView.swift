9ede8ec312d74174d19da5d42781ee9e26eeebbe
Pavel <pavel@holyavk.in>
2020-07-14 17:05:23 -0700

    Initial Commit

---
+++ SwiftUI PageViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI PageViewer
+//
+//  Created by Pavel Holyavkin on 7/14/20.
+//  Copyright © 2020 Pavel Holyavkin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
